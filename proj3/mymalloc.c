0cda7392e4fa53d3511b0ca88a767e1a03ef043a
Tim Tianyang Chen <soapcn@gmail.com>
2015-03-19 00:44:40 -0400

    Tested all four cases and some of the corner cases

--- proj3/mymalloc.c
+++ proj3/mymalloc.c
@@ -75,22 +75,28 @@ struct Node* find_best_fit(int size){
 void my_free(void *ptr){
 	struct Node* node_to_free=(struct Node*)(ptr-sizeof(struct Node));
 	int neighbours;
-	if((node_to_free!=head)&&(node_to_free!=tail)){//if freeing a node in the middle
-		printf("freeing the middle\n");
+	if(node_to_free->free){return;}
+	if((node_to_free!=head)&&(node_to_free!=tail)){//if freeing a node in the middle of two nodes
+		printf("freeing normal case\n");
 		neighbours=((node_to_free->previous_node->free)<<1)|(node_to_free->next_node->free);//00 01 10 11
 		switch(neighbours){
 			case 3://both are free
+				printf("merge three nodes\n");
 				node_to_free->previous_node->size=node_to_free->previous_node->size+node_to_free->size+node_to_free->next_node->size+2*sizeof(struct Node);
 				node_to_free->previous_node->next_node=node_to_free->next_node->next_node;
+				node_to_free->next_node->next_node->previous_node=node_to_free->previous_node;
 			break;
 			case 2://previous node is free
-				printf("merge free previous\n");
+				printf("merge previous free\n");
 				node_to_free->previous_node->size=node_to_free->previous_node->size+node_to_free->size+16;
 				node_to_free->previous_node->next_node=node_to_free->next_node;
+				node_to_free->next_node->previous_node=node_to_free->previous_node;
 			break;
 			case 1://next node is free
+				printf("merge next free\n");
 				node_to_free->size=node_to_free->size+node_to_free->next_node->size+16;
 				node_to_free->next_node=node_to_free->next_node->next_node;
+				node_to_free->next_node->next_node->previous_node=node_to_free;
 				node_to_free->free=1;
 			break;
 			case 0://none is free
@@ -100,19 +106,80 @@ void my_free(void *ptr){
 		}
 	}
 	else{
-		
-		
+		printf("corner cases\n");
+		if(head==tail){//only one node
+			head=NULL;
+			tail=head;
+		}	
+		else{	//more than one node	
+			if(node_to_free==head){
+				
+				if(node_to_free->next_node->free){//merge next
+					printf("freeing head and merge next\n");
+					head->size=head->size+head->next_node->size+sizeof(struct Node);
+					head->free=1;
+					head->next_node->next_node->previous_node=head;
+					head->next_node=head->next_node->next_node;
+				}
+				else{
+					printf("freeing head only\n");
+					head->free=1;//only free head node
+				}
+			}
+			if(node_to_free==tail){
+				printf("freeing tail\n");
+				if(tail->previous_node->free){
+					printf("previous is free\n");
+					if(tail->previous_node==head){
+						sbrk(0-tail->size-tail->previous_node->size-2*sizeof(struct Node));//shrink heap
+						head=NULL;//heap is gone
+						tail=head;
+						
+						printf("heap is gone bitch");
+					}
+					else{
+						sbrk(0-tail->size-sizeof(struct Node));//shrink heap
+						tail=tail->previous_node;
+						tail->next_node=NULL;
+						
+					}
+				}
+				else{
+					printf("previous is not free\n");
+					sbrk(0-tail->size-sizeof(struct Node));//shrink heap
+					tail=tail->previous_node;
+					tail->next_node=NULL;
+				}
+			}
+		}
 	}
 	
 	
 
 	
 }
+void print_list(){
+	struct Node *node=head;
+	while(node!=NULL){
+		if(node->free){
+			printf("free %d",node->size);
+		}
+		else{
+			printf("occupy %d",node->size);
+		}
+		if(node->next_node!=NULL){
+			printf(" --> "); 
+		}
+		node=node->next_node;
+	}
+	printf("\n");
+}
+
 
 
 int main(){
 	//init_list();
-	void* addr1;void* addr2;void* addr3;void* addr4;
+	void* addr1;void* addr2;void* addr3;void* addr4;void* addr5;
 	printf("The bottom of heap is %d\n",sbrk(0));
 	//initialize the first dummy node
 	
@@ -132,8 +199,35 @@ int main(){
 	printf("current addr is %d\n",addr4);
 	printf("current brk is %d\n",sbrk(0));
 	
+	addr5=my_bestfit_malloc(10);
+	printf("current addr is %d\n",addr5);
+	printf("current brk is %d\n",sbrk(0));
+	
+	print_list();
+	
+	printf("\nfreeing\n");
+	
+	
+	my_free(addr5);//free the second node
+	
+	print_list();
+	
+	my_free(addr4);//free the second node
+	print_list();
+	
+	my_free(addr1);//free the second node
+	//addr2=my_bestfit_malloc(63);
+	print_list();
+	
+	
 	my_free(addr2);//free the second node
+	print_list();
 	my_free(addr3);//free the second node
+	print_list();
+	
+	
+	
+	return;
 	addr2=my_bestfit_malloc(10);
 	
 	printf("current addr is %d\n",addr2);
@@ -142,6 +236,6 @@ int main(){
 	
 	printf("current addr is %d\n",addr2);
 	printf("current brk is %d\n",sbrk(0));
-
+    print_list();
 	return 1;
 }
